ede72b1bad5e7913a02e213de4b0b631bbc978d1
Chicken <hibary.taiwan@hotmail.com>
2022-07-01 22:55:45 +0800

    init

---
+++ src/Catcher.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catcher.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
