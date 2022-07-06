a836a8d5fbba72c28dcf9f9075af6d9dfca125dc
DukeLin <hibary.taiwan@gmail.com>
2022-07-06 07:24:06 -0700

    feat: login page

---
+++ src/Catcher.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catcher.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
