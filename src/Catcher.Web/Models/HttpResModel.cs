23e5a9f1e5397a695ffbb80db2a2793d79f25658
Chicken <hibary.taiwan@gmail.com>
2022-07-10 21:50:44 +0800

    feat: add login service

---
+++ src/Catcher.Web/Models/HttpResModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Catcher.Web.Models
+{
+    public class HttpResModel<T>
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+
+    public static class ApiCode
+    {
+        public const string Success = "0000";
+        public const string Fail = "9999";
+    }
+}
